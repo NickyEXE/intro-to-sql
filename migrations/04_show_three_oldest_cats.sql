7e2c50ad42c6e15ad81b0318a4b8b483a89b0833
NickyEXE <dovern42@gmail.com>
2021-03-30 19:07:47 -0400

    delete macavity

--- migrations/04_show_three_oldest_cats.sql
+++ migrations/04_show_three_oldest_cats.sql
@@ -2,7 +2,9 @@
 .width auto
 .headers on
 
-SELECT * FROM cats
+-- If you just want everything:
+-- SELECT * FROM cats
+SELECT cats.name, cats.title, cats.age FROM cats
 WHERE age > 5
 ORDER BY age DESC
 LIMIT 3;
