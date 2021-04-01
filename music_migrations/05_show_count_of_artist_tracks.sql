66856b773469a93d0fb1b45f16204d882c0d5afb
NickyEXE <dovern42@gmail.com>
2021-04-01 17:35:51 -0400

    update directions

--- music_migrations/05_show_count_of_artist_tracks.sql
+++ music_migrations/05_show_count_of_artist_tracks.sql
@@ -1,4 +1,4 @@
--- Show artist name, and a count of how many tracks they produced
+-- Show artist name, and a count of how many tracks they produced, ordered from most to least
 
 .mode column
 .width auto
